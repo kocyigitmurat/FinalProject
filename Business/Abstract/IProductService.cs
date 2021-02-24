82c1de1ec562090145e3e70d104152a9c425546e
Murat <Murat@DESKTOP-QDM0RD8>
2021-02-25 00:07:55 +0300

    25.02.2021 ders sonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -14,6 +14,8 @@ namespace Business.Abstract
         IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
-        IResult Add(Product product);        
+        IResult Add(Product product);
+        IResult Update(Product product);
+
     }
 }
